9ca56e406e976b2f80d6b1d887b450cf73764b2c
d1ffuz0r <d1fffuz0r@gmail.com>
2012-08-20 21:42:18 +0400

    added core files

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='django-tinyauth',
+    version='0.1',
+    packages=['django_tinyauth'],
+    url='https://github.com/d1ffuz0r/django-tinyauth',
+    license='GPL',
+    author='Roman Gladkov',
+    author_email='d1fffuz0r@gmail.com',
+    description='faster login to django admin interface',
+    zip_safe=False,
+    include_package_data=True,
+)
